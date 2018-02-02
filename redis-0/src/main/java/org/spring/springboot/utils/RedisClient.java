bee9b2fb04758b6783ae2a83d00b85add43df241
luckylau <laujunbupt0203@163.com>
2018-02-02 10:38:02 +0800

    优化

--- redis-0/src/main/java/org/spring/springboot/utils/RedisClient.java
+++ redis-0/src/main/java/org/spring/springboot/utils/RedisClient.java
@@ -65,7 +65,7 @@ public class RedisClient {
                     byte[] rawValue = valueSlz.serialize(value);
                     jedis.set(rawKey, rawValue);
                     if(timeout != null && unit != null){
-                        jedis.expireAt(rawKey, unit.toMillis(timeout));
+                        jedis.pexpire(rawKey, unit.toMillis(timeout));
                     }
                 }
             } catch (SerializationException e) {

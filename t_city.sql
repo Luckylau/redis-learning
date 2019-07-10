4b66dc1e368be5792148fe3e005ec37ce92b68d0
luckylau <laujunbupt0203@163.com>
2019-07-10 22:28:59 +0800

    更新sql

--- t_city.sql
+++ t_city.sql
@@ -1,14 +1,9 @@
 CREATE TABLE `t_city` (
-`id`  bigint(20) NOT NULL AUTO_INCREMENT ,
-`province`  varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' ,
-`cityEntity`  varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' ,
-`description`  varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' ,
-`create_time`  timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-`modify_time`  timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
-PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
-AUTO_INCREMENT=46
-ROW_FORMAT=DYNAMIC
-;
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `province` varchar(255) NOT NULL DEFAULT '',
+  `cityEntity` varchar(255) NOT NULL DEFAULT '',
+  `description` varchar(255) NOT NULL DEFAULT '',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modify_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4

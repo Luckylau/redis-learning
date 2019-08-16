a6867fdf69761907d1f63adafc47c56c80c36856
luckylau <laujunbupt0203@163.com>
2019-08-16 08:49:10 +0800

    数据库

--- t_city.sql
+++ t_city.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `t_city` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `province` varchar(255) NOT NULL DEFAULT '',
-  `cityEntity` varchar(255) NOT NULL DEFAULT '',
+  `city` varchar(255) NOT NULL DEFAULT '',
   `description` varchar(255) NOT NULL DEFAULT '',
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modify_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,

68711b2e3440954dbb20d9595c53267bac21fa62
luckylau <laujunbupt0203@163.com>
2022-04-16 22:31:13 +0800

    feat(redis-3): springboot 2.6.5 support druid starter

--- redis-3/src/main/java/org/spring/springboot/Redis3.java
+++ redis-3/src/main/java/org/spring/springboot/Redis3.java
@@ -1,12 +1,16 @@
 package org.spring.springboot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 /**
  * @author luckylau
  * @date 2019/8/10
  */
-
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Redis3 {
     public static void main(String[] args) {
-        
+        SpringApplication.run(Redis3.class, args);
     }
 }
